16ff3a951cfb5464a5932c56d0f34f07fbc824a2
ClaireHien <clairehien.osmont@gmail.com>
2019-10-10 11:46:39 +0200

    DarkTrader1

--- lib/Dark_Trader.rb
+++ lib/Dark_Trader.rb
@@ -5,3 +5,36 @@ def page_lien
     page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
     return page
 end 
+
+def dark_trader
+    page = page_lien
+
+    #Tous les prix a récupérer dans la table_price
+    table_price = []
+    page.css('a.price').each do |node|
+        table_price << node.text
+    end
+
+    #Tous les symbol à récupérer dans la table_symbol
+    table_symbol = []
+    page.css('td.col-symbol').each do |node2|
+        table_symbol << node2.text
+    end
+
+    nbr = table_symbol.size
+    n = 0
+    table = []
+
+    #pour ranger les données dans des hash dans un tableau
+    nbr.times do
+        hash = {}
+        hash["#{table_symbol[n]}"] = table_price[n]
+        table << hash
+        n = n + 1
+    end
+    
+   # puts table
+
+end
+
+dark_trader
